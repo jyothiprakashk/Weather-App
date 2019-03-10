9e834c8c52608ec9fc68edbc1e546471cb3deee8
jyojthiprakash097 <jyothiprakash097@gmail.com>
2019-03-10 14:59:39 +0530

    'four'

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
